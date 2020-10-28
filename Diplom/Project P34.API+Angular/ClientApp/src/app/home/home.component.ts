f847a7b6b13a21510bb3f2c5b32a884ded280195
veremeychukden <veremeychukden@gmail.com>
2020-10-28 20:45:31 +0200

    first commit

--- Diplom/Project P34.API+Angular/ClientApp/src/app/home/home.component.ts
+++ Diplom/Project P34.API+Angular/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var name: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    new name();
+  }
+  title = 'app-js';
 }
